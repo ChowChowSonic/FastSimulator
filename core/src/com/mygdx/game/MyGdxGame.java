21b65e363345aee9b250b54fe2615294f5e65d58
ChowChowSonic <joeyant1367@gmail.com>
2020-08-18 18:20:05 -0400

    Updated menu, still nonfunctional
    Menu now looks better and uses the default, reccomended button type instead of a custom button type. However, it still does not work

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,19 +6,18 @@ import java.util.ArrayList;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-import Entities.Enemy;
 import Entities.Player;
 
 public class MyGdxGame extends ApplicationAdapter {
@@ -26,38 +25,81 @@ public class MyGdxGame extends ApplicationAdapter {
 	 * framework class for the game. Does not do much aside from 
 	 * supporting the other classes and allowing them to work
 	 */
-	SpriteBatch batch;
-	TextureRegion region;
-	OrthographicCamera camera;
-
-	TiledGameMap gameMap;
-	Player p;
-	float deltaX, deltaY;
-	float w;
-	float h;
-	boolean maploaded = false;
-	ArrayList<CustomButton> buttons = new ArrayList<CustomButton>();
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private TiledGameMap gameMap;
+	private Player p;
+	private Stage menu;
+	private Skin defaultlook;
+	
+	private float w, h;
+	private double loadtime;
+	private boolean maploaded = false;
 	public enum gamestate{menu, level, hub};
-	gamestate currentstate = gamestate.menu;
+	public gamestate currentstate = gamestate.menu;
 
 	@Override
 	public void create () {
+		//Load in all the required components  
+		loadtime = System.currentTimeMillis();
+		defaultlook = new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
 		batch = new SpriteBatch();
-		region = new TextureRegion();
+		new TextureRegion();
 		Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
 		Gdx.graphics.setWindowedMode(screensize.width, screensize.height);
+		
+		//load in the main menu
+		Gdx.input.setInputProcessor(menu);
+		menu = new Stage(new ScreenViewport());
+		Button QueuedButton = new TextButton("Play",defaultlook);
+		QueuedButton.setSize(500, 200);
+		QueuedButton.setPosition(menu.getWidth()/3, menu.getHeight()/1.4f);
+		QueuedButton.addListener(new InputListener() {
+			@Override
+		    public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+				currentstate = gamestate.level;
+		    }
+		    @Override
+		    public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+		        currentstate = gamestate.level;
+		        return true;
+		    }
+		});
+		
+		Button Options = new TextButton("Options",defaultlook);
+		Options.setSize(500, 200);
+		Options.setPosition(menu.getWidth()/3, menu.getHeight()/2.5f);
+		Options.addListener(new InputListener() {
+			@Override
+		    public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+		        
+		    }
+		    @Override
+		    public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+		        return true;
+		    }
+		});
+		
+		Button ExitButton = new TextButton("Exit",defaultlook);
+		ExitButton.setSize(500, 200);
+		ExitButton.setPosition( menu.getWidth()/3, menu.getHeight()/12);
+		ExitButton.addListener(new InputListener() {
+			@Override
+		    public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+		        
+		    }
+		    @Override
+		    public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+		        System.exit(0);
+		        return true;
+		    }
+		});
+		
+		menu.addActor(QueuedButton);
+		menu.addActor(Options);
+		menu.addActor(ExitButton);
 
-		camera = new OrthographicCamera();
-		w = Gdx.graphics.getWidth()/1.75f;
-		h = Gdx.graphics.getHeight()/1.75f;
-		camera.setToOrtho(false,w,h);
-		camera.update();
-		CustomButton button = new CustomButton(screensize.width/3, 500, 500, 200, "PlayEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
-		buttons.add(button);
-		button = new CustomButton(screensize.width/3, 300, 500, 200, "Options");
-		buttons.add(button);
-		button = new CustomButton(screensize.width/3, 100, 500, 200, "Exit");
-		buttons.add(button);
+		System.out.println(loadtime - System.currentTimeMillis());
 	}
 
 	@Override
@@ -66,13 +108,13 @@ public class MyGdxGame extends ApplicationAdapter {
 			Gdx.gl.glClearColor( 0, 0, 0, 0 );//clears the background
 			  Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );//clears the depth buffer bit
 			  	//...Dont ask me what that is all I know is that these two lines of code stop everything from flickering.
-			for(CustomButton b : buttons) { 
-				if(b!= null)b.draw();
-			}
+			menu.act();
+			menu.draw();
 		}
+		//if(loadtime - System.currentTimeMillis() < 10000) buttons.get(0).onclick();
 		//game state
 		else if(currentstate == gamestate.level) {
-			if(!maploaded) loadmap();
+			if(!maploaded) loadmap("ExterminationDemo.tmx");
 			float deltatime = Gdx.graphics.getDeltaTime();
 			Gdx.gl.glClearColor(49/255.0f, 162.0f/255, 242.0f/255, 100);
 			Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
@@ -98,15 +140,31 @@ public class MyGdxGame extends ApplicationAdapter {
 			gameMap.render(camera, batch);
 		}
 	}
-	private void loadmap() {
-		gameMap = new TiledGameMap("ExterminationDemo.tmx", "aFinal Fantasy VII Remake - [ Battle Theme ] Let the Battles Begin (OST).mp3");// Map to be loaded
+	private void loadmap(String mapname) {
+		try {
+		gameMap = new TiledGameMap(mapname, "aFinal Fantasy VII Remake - [ Battle Theme ] Let the Battles Begin (OST).mp3");// Map to be loaded
 		//gameMap = new TiledGameMap(); //...Or use a default map
 		p=(Player) gameMap.getEntitybyType(new Player());
 		int[] spawnpoint = gameMap.getPlayerSpawnPoint();
 		p.setX(spawnpoint[0]);
 		p.setY(spawnpoint[1]);
+		
+		//load the camera
+		camera = new OrthographicCamera();
+		w = Gdx.graphics.getWidth()/1.75f;
+		h = Gdx.graphics.getHeight()/1.75f;
+		camera.setToOrtho(false,w,h);
+		camera.update();
+		
+		//confirm map was loaded
 		maploaded = true;
-	}
+		return;
+		}catch(Exception e) {
+			this.dispose();
+			e.printStackTrace();
+			System.exit(1);
+		}
+		}
 	/**
 	 * Extracts the player from the mission, cues the mission successful/fail theme, then displays the mission summary menu.
 	 * 
@@ -128,7 +186,9 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void dispose () {
+		if (batch!= null)
 		batch.dispose();
+		if(gameMap != null)
 		gameMap.dispose();
 	}
 }
