4f135c4a2ca7d65c85e5d2b13c850f32de7ebdbf
ChowChowSonic <joeyant1367@gmail.com>
2020-10-18 11:45:52 -0400

    Updates to assets
    Added more assets and updated some old ones

---
+++ desktop/bin/main/Sky.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="Sky" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="Sky.png" trans="00aaff" width="32" height="32"/>
+ <tile id="0" type="1"/>
+</tileset>
